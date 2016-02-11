b1cf6e27286a3a2ca0985e61ff4e6dc70eca259c
nic netzel and ryan brown <browneryan87@gmail.com>
2016-02-11 10:35:20 -0800

    Add save, getall, deletall functions, remove price property.

--- src/Cd.php
+++ src/Cd.php
@@ -4,28 +4,12 @@
         private $title;
         private $artist;
         private $cover_art;
-        private $price;
 
-        function __construct($album_name, $band_name, $image_path, $album_price = 10.99)
+        function __construct($album_name, $band_name, $image_path)
         {
             $this->title = $album_name;
             $this->artist = $band_name;
             $this->cover_art = $image_path;
-            $this->price = $album_price;
-        }
-
-        function setPrice($new_price)
-        {
-            $float_price = (float) $new_price;
-            if ($float_price != 0) {
-                $formatted_price = number_format($float_price, 2);
-                $this->price = $formatted_price;
-            }
-        }
-
-        function getPrice()
-        {
-            return $this->price;
         }
 
         function setTitle($new_title)
@@ -56,6 +40,21 @@
         function getCoverArt()
         {
             return $this->cover_art;
+        }
+
+        function saveCD()
+        {
+            array_push($_SESSION['list_of_CDs'], $this);
+        }
+
+        static function getAll()
+        {
+            return $_SESSION['list_of_CDs'];
+        }
+        
+        static function deleteAll()
+        {
+            $_SESSION['list_of_CDs']= array();
         }
 
     }
