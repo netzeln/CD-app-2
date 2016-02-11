94971683d52affe724b988e8160c8b83f757a06d
nic netzel and ryan brown <browneryan87@gmail.com>
2016-02-11 12:01:45 -0800

    add add-new-cd functionality

--- app/app.php
+++ app/app.php
@@ -13,20 +13,29 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=>__DIR__.'/../views'
     ));
 
-
-
     $app->get("/", function() use ($app) {
-      $first_cd = new CD("The Birthday Concert", "Jaco Pastorius", "img/birthday.jpg");
-      $second_cd = new CD("Blackstar", "David Bowie", "img/blackstar.jpg");
-      $third_cd = new CD("Bright Size Life", "Pat Metheney", "img/bright.jpg");
-      $fourth_cd = new CD("Once Upon a Time in Shaolin", "Wu-Tang Clan", "img/wutang.jpeg", 2000000);
-      $fifth_cd = new  CD("Ten", "Pearl Jam", "img/ten.jpg", 5);
-      $cds = array($first_cd, $second_cd, $third_cd, $fourth_cd, $fifth_cd);
+    //   $first_cd = new CD("The Birthday Concert", "Jaco Pastorius", "img/birthday.jpg");
+    //   $second_cd = new CD("Blackstar", "David Bowie", "img/blackstar.jpg");
+    //   $third_cd = new CD("Bright Size Life", "Pat Metheney", "img/bright.jpg");
+    //   $fourth_cd = new CD("Once Upon a Time in Shaolin", "Wu-Tang Clan", "img/wutang.jpeg", 2000000);
+    //   $fifth_cd = new  CD("Ten", "Pearl Jam", "img/ten.jpg", 5);
+    //   $cds = array($first_cd, $second_cd, $third_cd, $fourth_cd, $fifth_cd);
 
-      $cd_list = $cds;
-
+        $cd_list = $_SESSION['list_of_CDs'];
 
         return $app['twig']->render('index.html.twig', array('cds'=> $cd_list));
+    });
+
+    $app->get("/add_CD", function() use ($app) {
+
+        return $app['twig']->render('add_CD.html.twig');
+    });
+
+    $app->post("/new_CD", function() use ($app){
+        $newCD = new CD($_POST['title'], $_POST['artist'], $_POST['cover_image']);
+        $newCD->saveCD();
+
+        return $app['twig']->render('new_CD.html.twig', array('newCD'=>$newCD));
     });
 
     return $app;
